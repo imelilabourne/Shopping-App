8c49e021c99656359aa25cf4e213f35d9c127397
Sean Gabriel <joannaseangabriel@gmail.com>
2020-10-05 17:06:47 +0800

    Sean_Update Seller

---
+++ src/app/shopping-app/model/Products.ts
@@ -0,0 +1,8 @@
+export interface Products {
+    id : number;
+    productname : string;
+    productdetails : string;
+    productprice : number;
+    productquantity : number;
+    status: string;
+}
--- src/app/shopping-app/seller/seller.component.ts
+++ src/app/shopping-app/seller/seller.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Products} from '../model/Products'
 @Component({
   selector: 'app-seller',
   templateUrl: './seller.component.html',
@@ -7,9 +8,95 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerComponent implements OnInit {
 
-  constructor() { }
+  products: Products[];
+  upproducts: Products[] = [];
+ // selectedProduct : Subject<any> = new Subject;
+  total:number = 0;
+  delit:number = 0;
+
+  constructor() { 
+    this.products = [
+      {
+        id: 1,
+        productname: "Nike Court Air Zoom",
+        productdetails:"Colour Shown: Dark Atomic Teal/Beyond Pink/White",
+        productprice:8000,
+        productquantity:5,
+        status: "Limited Stocks"
+       
+      },
+      {
+        id: 2,
+        productname: "Nike Court Air Zoom",
+        productdetails:"Colour Shown: Dark Atomic Teal/Beyond Pink/White",
+        productprice:8000,
+        productquantity:5,
+        status: "Limited Stocks"
+       
+      },
+      {
+        id: 3,
+        productname: "Nike Court Air Zoom",
+        productdetails:"Colour Shown: Dark Atomic Teal/Beyond Pink/White",
+        productprice:8000,
+        productquantity:5,
+        status: "Limited Stocks"
+       
+      }
+    ];
+   }
 
   ngOnInit() {
+    this.totalPrice();
+  }
+
+ // getpopup(det) {
+  // this.selectedProduct.next(det);
+//}
+
+
+  delpopup(pid){
+    console.log(pid);
+    for(var i=0;i<this.products.length;i++){
+      if(this.products[i].id === pid)
+      {  
+        this.products.splice(i,1);
+      }           
+    }
+    this.totalPrice();
+    console.log(this.products);
+  }
+
+
+  totalPrice(){
+    this.total = 0;
+    for(var i=0;i<this.products.length;i++){
+      this.total += (this.products[i].productprice * this.products[i].productquantity);
+    }
+  }
+
+  add(pid){
+    console.log(pid);
+    for(var i=0;i<this.products.length;i++){
+      if(this.products[i].id === pid)
+      {  
+        this.products[i].productquantity += 1;
+      }           
+    }
+    this.totalPrice();
+    console.log(this.products);
+  }
+
+  del(pid){
+    console.log(pid);
+    for(var i=0;i<this.products.length;i++){
+      if(this.products[i].id === pid)
+      {  
+        this.products[i].productquantity -= 1;
+      }           
+    }
+    this.totalPrice();
+    console.log(this.products);
   }
 
 }
