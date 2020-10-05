10c27e8b0e2a7f421d550a25441ddf7370a83d7c
imelilabourne <elilabourne.detorres@gmail.com>
2020-10-05 14:44:50 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BuyerComponent } from './shopping-app/buyer/buyer/buyer.component';
+import { SellerComponent } from './shopping-app/seller/seller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyerComponent,
+    SellerComponent
   ],
   imports: [
     BrowserModule
